5627cf636c190c6f3c22c6661573c04ac0db1d51
Robert Moss <rmoss92@gmail.com>
2023-11-17 18:29:41 -0800

    Tie break actions forcing 0 vertical rate

--- src/pomdp.jl
+++ src/pomdp.jl
@@ -3,7 +3,7 @@
     dh_rel_range::Vector{Real} = [-1, 1]  # initial relative vertical rates [m²]
     ddh_max::Real = 1.0                   # vertical acceleration limit [m/s²]
     τ_max::Real = 40                      # max time to closest approach [s]
-    actions::Vector{Real} = [-5, 0.0, 5]  # relative vertical rate actions [m/s²]
+    actions::Vector{Real} = [0.0, -5, 5]  # relative vertical rate actions [m/s²] (tie break zero)
     collision_threshold::Real = 50        # collision threshold [m]
     reward_collision::Real = -100         # reward obtained if collision occurs
     reward_reversal::Real = -25           # reward obtained if action reverses direction (e.g., from +5 to -5)
